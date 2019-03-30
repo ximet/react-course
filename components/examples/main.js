956c58f4e77030489a69e8cda0e073c92cc5c6a5
ximet <dq.ximet@gmail.com>
2019-03-30 22:01:27 +0300

    add mainjs

--- components/examples/main.js
+++ components/examples/main.js
@@ -6,7 +6,46 @@ function Button_Functional (props){
     )
 }
 
+////////////////////////////////////////////////////
 
-const button = (props) => {
-    
+const inputComponent = (props) => {
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input onChange={props.onChange} />
+        </div>
+    )
+}
+
+////////////////////////////////////////////////////
+
+
+import PropTypes from 'prop-types';
+
+function Banner(props) {
+    const { message, openDialog, label } = props;
+  
+    return (
+      <div>
+        <div>{message}</div>
+        <button onClick={props.openDialog}>{props.label}</button>
+      </div>
+    );
+}
+
+Banner.defaultProps = {
+    message: 'Problem'
+};
+
+Banner.propTypes = {
+    /**
+     *
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     */
+    openDialog: PropTypes.func,
+    /**
+     */
+    message: PropTypes.string.isRequired,
 }
